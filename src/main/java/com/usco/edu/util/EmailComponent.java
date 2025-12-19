6f87593d4cc72cd931c78f3aabb2882844e5151c
JohnGuerreroCor <john.guerrero@usco.edu.co>
2025-12-19 14:57:57 -0500

    feature: integración mejoras de estructura

--- src/main/java/com/usco/edu/util/EmailComponent.java
+++ src/main/java/com/usco/edu/util/EmailComponent.java
@@ -20,25 +20,25 @@ import com.usco.edu.dto.Email;
 
 @Component
 public class EmailComponent {
-	
-	
+
 	Logger logger = LoggerFactory.getLogger(EmailComponent.class);
-	
+
 	public void enviar(Email email) {
-		
+
 		this.enviar(email, false);
 	}
-	
+
 	public void enviar(Email email, boolean lanzarError) {
-		
+
 		MimeMessagePreparator messagePreparator = mimeMessage -> {
 
 			MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
 			messageHelper.setTo(email.getDestinatario());
+			messageHelper.setFrom("carnetizacion1@usco.edu.co");
 			messageHelper.setSubject(email.getAsunto());
-			
+
 			String mje = this.build(email);
-			messageHelper.setText( mje , true);
+			messageHelper.setText(mje, true);
 		};
 
 		try {
@@ -49,17 +49,17 @@ public class EmailComponent {
 		} catch (MailException e) {
 			logger.error(email.toString());
 			e.printStackTrace();
-			
-			if(lanzarError) throw e;
+
+			if (lanzarError)
+				throw e;
 		}
 	}
-	
-	
-	private String build(Email email ) throws Exception {
 
-		String plantillaCorreo = this
-				.obtenerTextoDeArchivo(new ClassPathResource("static/plantilla_email/notificacion_programa.html").getFile());
-		
+	private String build(Email email) throws Exception {
+
+		String plantillaCorreo = this.obtenerTextoDeArchivo(
+				new ClassPathResource("static/plantilla_email/notificacion_programa.html").getFile());
+
 		plantillaCorreo = plantillaCorreo.replaceAll(":nombrePersona", email.getNombreDestinatario());
 		plantillaCorreo = plantillaCorreo.replaceAll(":ruta", email.getHash());
 		plantillaCorreo = plantillaCorreo.replaceAll(":textoUno", email.getTextoUno());
@@ -69,13 +69,10 @@ public class EmailComponent {
 		plantillaCorreo = plantillaCorreo.replaceAll(":textoTres", email.getTextoTres());
 		plantillaCorreo = plantillaCorreo.replaceAll(":footer", email.getFooter());
 		plantillaCorreo = plantillaCorreo.replaceAll(":programa", email.getPrograma());
-		
-		
+
 		return plantillaCorreo;
 	}
-	
-	
-	
+
 	private String obtenerTextoDeArchivo(File file) {
 
 		FileReader fr = null;
@@ -99,9 +96,7 @@ public class EmailComponent {
 		}
 		return contenido;
 	}
-	
-	
-	
+
 	private JavaMailSender crearJavaMailSender() {
 
 		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
@@ -123,27 +118,24 @@ public class EmailComponent {
 
 		return mailSender;
 	}
-	
-	
-	
-	//---- metodo para seleccionar un correo de forma aleatoria
+
+	// ---- metodo para seleccionar un correo de forma aleatoria
 	private UserPass getEmailRandom() {
-		
-		UserPass emails[] = { 
-				new UserPass("autenticacion.usco1@usco.edu.co","4untent1c4cion1"),
-				new UserPass("autenticacion.usco2@usco.edu.co","4untent1c4cion2"),
-				new UserPass("autenticacion.usco3@usco.edu.co","4untent1c4cion3"),
-				new UserPass("autenticacion.usco4@usco.edu.co","4untent1c4cion4"),
-				new UserPass("autenticacion.usco5@usco.edu.co","4untent1c4cion5"),
-				new UserPass("autenticacion.usco6@usco.edu.co","4untent1c4cion6"),
-				new UserPass("autenticacion.usco7@usco.edu.co","4untent1c4cion7"),
-				new UserPass("autenticacion.usco8@usco.edu.co","4untent1c4cion8"),
-				new UserPass("autenticacion.usco9@usco.edu.co","4untent1c4cion9"),
-				new UserPass("autenticacion.usco10@usco.edu.co","4untent1c4cion10"),
-				new UserPass("autenticacion.usco11@usco.edu.co","4untent1c4cion11"),
-				new UserPass("autenticacion.usco12@usco.edu.co","4untent1c4cion12"),
-				new UserPass("autenticacion.usco13@usco.edu.co","4untent1c4cion13"),
-				};
+
+		UserPass emails[] = { new UserPass("autenticacion.usco1@usco.edu.co", "ukhr tido yopp cswz"),
+				new UserPass("autenticacion.usco2@usco.edu.co", "hllq unam viev fsfr"),
+				new UserPass("autenticacion.usco3@usco.edu.co", "xwuc jnwi kiyf sznj"),
+				new UserPass("autenticacion.usco4@usco.edu.co", "fegi xzct onud pxht"),
+				new UserPass("autenticacion.usco5@usco.edu.co", "urxh tqbt kyhz crae"),
+				new UserPass("autenticacion.usco6@usco.edu.co", "vxmg nrdh ixxu ffen"),
+				new UserPass("autenticacion.usco7@usco.edu.co", "mkcu euou tlzv gqsx"),
+				new UserPass("autenticacion.usco8@usco.edu.co", "jtjz kdnq mjif fbkb"),
+				new UserPass("autenticacion.usco9@usco.edu.co", "hdmu drpd opmf dzfs"),
+				new UserPass("autenticacion.usco10@usco.edu.co", "mkup welc xkpx lncd"),
+				new UserPass("autenticacion.usco12@usco.edu.co", "lyep tdfa jcgm otya"),
+				new UserPass("autenticacion.usco13@usco.edu.co", "hlrk rdpc nfpr dogg"),
+				// new UserPass("autenticacion.usco11@usco.edu.co","4untent1c4cion11"),
+		};
 
 		Random random = new Random();
 		int n = random.nextInt(emails.length);
@@ -153,18 +145,16 @@ public class EmailComponent {
 		return email;
 	}
 
-
-	//---- clase para para establecer el usuario y contrasena del correo---//
+	// ---- clase para para establecer el usuario y contrasena del correo---//
 	private class UserPass {
-		
+
 		String user;
 		String pass;
-		
+
 		public UserPass(String user, String pass) {
 			this.user = user;
 			this.pass = pass;
 		}
 	}
-
 
 }
--- src/main/java/com/usco/edu/util/EmailTokenComponent.java
+++ src/main/java/com/usco/edu/util/EmailTokenComponent.java
@@ -20,25 +20,25 @@ import com.usco.edu.dto.EmailToken;
 
 @Component
 public class EmailTokenComponent {
-	
-Logger logger = LoggerFactory.getLogger(EmailComponent.class);
-	
+
+	Logger logger = LoggerFactory.getLogger(EmailComponent.class);
+
 	public void enviar(EmailToken email) {
-		
+
 		this.enviar(email, false);
 	}
-	
+
 	public void enviar(EmailToken email, boolean lanzarError) {
-		
+
 		MimeMessagePreparator messagePreparator = mimeMessage -> {
 
 			MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
 			messageHelper.setTo(email.getEmail());
 			messageHelper.setFrom("autenticacion.usco1@usco.edu.co");
 			messageHelper.setSubject("Código Inicio de Sesión - Módulo Control de Acceso");
-			
+
 			String mje = this.build(email);
-			messageHelper.setText( mje , true);
+			messageHelper.setText(mje, true);
 		};
 
 		try {
@@ -49,27 +49,24 @@ Logger logger = LoggerFactory.getLogger(EmailComponent.class);
 		} catch (MailException e) {
 			logger.error(email.toString());
 			e.printStackTrace();
-			
-			if(lanzarError) throw e;
+
+			if (lanzarError)
+				throw e;
 		}
 	}
-	
-	
-	private String build(EmailToken email ) throws Exception {
 
-		String plantillaCorreo = this
-				.obtenerTextoDeArchivo(new ClassPathResource("static/plantilla_email/plantilla-token-email.html").getFile());
-		
+	private String build(EmailToken email) throws Exception {
+
+		String plantillaCorreo = this.obtenerTextoDeArchivo(
+				new ClassPathResource("static/plantilla_email/plantilla-token-email.html").getFile());
+
 		plantillaCorreo = plantillaCorreo.replaceAll(":token", email.getToken());
 		plantillaCorreo = plantillaCorreo.replaceAll(":email", email.getEmail());
 		plantillaCorreo = plantillaCorreo.replaceAll(":nombre", email.getNombre());
-		
-		
+
 		return plantillaCorreo;
 	}
-	
-	
-	
+
 	private String obtenerTextoDeArchivo(File file) {
 
 		FileReader fr = null;
@@ -93,9 +90,7 @@ Logger logger = LoggerFactory.getLogger(EmailComponent.class);
 		}
 		return contenido;
 	}
-	
-	
-	
+
 	private JavaMailSender crearJavaMailSender() {
 
 		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
@@ -117,27 +112,24 @@ Logger logger = LoggerFactory.getLogger(EmailComponent.class);
 
 		return mailSender;
 	}
-	
-	
-	
-	//---- metodo para seleccionar un correo de forma aleatoria
+
+	// ---- metodo para seleccionar un correo de forma aleatoria
 	private UserPass getEmailRandom() {
-		
-		UserPass emails[] = { 
-				new UserPass("autenticacion.usco1@usco.edu.co","4untent1c4cion1"),
-				new UserPass("autenticacion.usco2@usco.edu.co","4untent1c4cion2"),
-				new UserPass("autenticacion.usco3@usco.edu.co","4untent1c4cion3"),
-				new UserPass("autenticacion.usco4@usco.edu.co","4untent1c4cion4"),
-				new UserPass("autenticacion.usco5@usco.edu.co","4untent1c4cion5"),
-				new UserPass("autenticacion.usco6@usco.edu.co","4untent1c4cion6"),
-				new UserPass("autenticacion.usco7@usco.edu.co","4untent1c4cion7"),
-				new UserPass("autenticacion.usco8@usco.edu.co","4untent1c4cion8"),
-				new UserPass("autenticacion.usco9@usco.edu.co","4untent1c4cion9"),
-				new UserPass("autenticacion.usco10@usco.edu.co","4untent1c4cion10"),
-				new UserPass("autenticacion.usco11@usco.edu.co","4untent1c4cion11"),
-				new UserPass("autenticacion.usco12@usco.edu.co","4untent1c4cion12"),
-				new UserPass("autenticacion.usco13@usco.edu.co","4untent1c4cion13"),
-				};
+
+		UserPass emails[] = { new UserPass("autenticacion.usco1@usco.edu.co", "ukhr tido yopp cswz"),
+				new UserPass("autenticacion.usco2@usco.edu.co", "hllq unam viev fsfr"),
+				new UserPass("autenticacion.usco3@usco.edu.co", "xwuc jnwi kiyf sznj"),
+				new UserPass("autenticacion.usco4@usco.edu.co", "fegi xzct onud pxht"),
+				new UserPass("autenticacion.usco5@usco.edu.co", "urxh tqbt kyhz crae"),
+				new UserPass("autenticacion.usco6@usco.edu.co", "vxmg nrdh ixxu ffen"),
+				new UserPass("autenticacion.usco7@usco.edu.co", "mkcu euou tlzv gqsx"),
+				new UserPass("autenticacion.usco8@usco.edu.co", "jtjz kdnq mjif fbkb"),
+				new UserPass("autenticacion.usco9@usco.edu.co", "hdmu drpd opmf dzfs"),
+				new UserPass("autenticacion.usco10@usco.edu.co", "mkup welc xkpx lncd"),
+				new UserPass("autenticacion.usco12@usco.edu.co", "lyep tdfa jcgm otya"),
+				new UserPass("autenticacion.usco13@usco.edu.co", "hlrk rdpc nfpr dogg"),
+				// new UserPass("autenticacion.usco11@usco.edu.co","4untent1c4cion11"),
+		};
 
 		Random random = new Random();
 		int n = random.nextInt(emails.length);
@@ -147,13 +139,12 @@ Logger logger = LoggerFactory.getLogger(EmailComponent.class);
 		return email;
 	}
 
-
-	//---- clase para para establecer el usuario y contrasena del correo---//
+	// ---- clase para para establecer el usuario y contrasena del correo---//
 	private class UserPass {
-		
+
 		String user;
 		String pass;
-		
+
 		public UserPass(String user, String pass) {
 			this.user = user;
 			this.pass = pass;
